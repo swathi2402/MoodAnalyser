84934ef5afe5d2f4178e13784eb8c02684bf781a
Swathi <swathi.h@perfios.com>
2021-09-10 20:35:17 +0530

    ADDED: Custom Exception file to handle Null and Empty cases

---
+++ src/main/java/com/bridgelabz/moodanalyser/MoodAnalysisException.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.moodanalyser;
+
+public class MoodAnalysisException extends Exception {
+	
+	enum ExceptionType {
+		ENTERED_NULL, ENTERED_EMPTY
+	}
+	ExceptionType type;
+
+	public MoodAnalysisException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
+}
